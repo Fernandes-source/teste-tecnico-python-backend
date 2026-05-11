35d83703152382ca378147657c2ffcc70cba416f
= <=>
2026-05-11 17:33:40 -0400

    criando logica raiz do app

---
+++ app/services/foco.py
@@ -0,0 +1,27 @@
+registros = []
+
+def acrescentar_registros(dado):
+    registros.append(dado)
+
+def mostrar_registros():
+    return registros
+
+def medir-registros():
+    if not registros:
+        return {"msg": "Não há registros de foco"}
+    
+    media = sum(r.nivel_foco for r in registros)/ len(registros)
+    tempo_total = sum(r.tempo_minutos for r in registros)
+
+    if media < 3:
+        feedback = "Faça pausas maiores e mecha menos no celular"
+    elif media > 4:
+        feedback = "Boa perfomance, continue"
+    else:
+        feedback = "equilibrado"
+
+    return{
+        "media_foco": media,
+        "tempo_total": tempo_total,
+        "feedback": feedback
+    }
