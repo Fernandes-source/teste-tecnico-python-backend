df5861ea848eb1f9241f228a6bd3c3e4bbf87b54
= <=>
2026-05-11 21:00:52 -0400

    Corrigindo erro de dados de entrada

--- app/models/dados_foco.py
+++ app/models/dados_foco.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 
 class RegistroFoco(BaseModel):
-    nivel_foco: int = Field(..., gt=1, lt=5)
+    nivel_foco: int = Field(..., gt=0, lt=6)
     tempo_minutos: int
     comentario: str
